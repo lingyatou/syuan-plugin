8d88a91bad9240989a143289cca1be817fb50afa
源Syuan <2331329306@qq.com>
2025-08-09 14:27:13 +0800

    3

--- tools/index.js
+++ tools/index.js
@@ -1,5 +1,6 @@
 import { sshData } from './data.js'
 import { pluginPath, rootPath, dataPath } from './path.js'
+import { versionInfo } from './version.js'
+import { cfgdata } from './cfg.js'
 
-
-export { sshData, pluginPath, rootPath, dataPath }
+export { sshData, pluginPath, rootPath, dataPath, versionInfo, cfgdata }
