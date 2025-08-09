812bd73680f49506d73e9e8c53ce34fb76642735
源Syuan <2331329306@qq.com>
2025-08-09 14:55:10 +0800

    5

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ export * from './apps/index.js'
 if (!segment?.button)
     segment.button = () => ""
 
-if (Bot?.logger?.info) {
-    Bot.logger.info('---------^_^---------')
-    Bot.logger.info(versionInfo.getInitMessage())
-} else {
-    console.log(versionInfo.getInitMessage())
-}
+logger.info('---------```````^_^```````---------')
+logger.info(versionInfo.getInitMessage())
 
 // 移除未定义的Index.init调用
 setTimeout(Index.init, 1000)
