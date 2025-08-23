b1223f779f76070f2b802fa9d8ab155768db96b5
源Syuan <2331329306@qq.com>
2025-08-23 08:58:07 +0800

    22

--- apps/update.js
+++ apps/update.js
@@ -1,4 +1,4 @@
-import { rootPath, dataPath, cfgdata } from '../tools/index.js'
+import { pluginPath, rootPath, dataPath } from '../tools/index.js'
 import { exec } from 'child_process'
 
 
@@ -23,9 +23,9 @@ export class Update extends plugin {
     }
 
     async SyuanUpdate(e) {
-        //使用rootPath,在这个目录下进行git更新
+        //使用pluginPath,在这个目录下进行git更新
         e.reply('[Syuan-plugin]开始更新插件，请稍等...')
-        exec(`git -C "${rootPath}" pull`, (err, stdout, stderr) => {
+        exec(`git -C "${pluginPath}" pull`, (err, stdout, stderr) => {
             if (err) {
                 e.reply('更新失败：' + err.message)
                 return
@@ -35,10 +35,10 @@ export class Update extends plugin {
     }
 
     async SyuanForceUpdate(e) {
-        //使用rootPath,在这个目录下进行git忽略本地改动更新
+        //使用pluginPath,在这个目录下进行git忽略本地改动更新
         e.reply('[Syuan-plugin]开始强制更新插件，请稍等...')
         // 强制更新会丢失本地改动
-        const cmd = `git -C "${rootPath}" reset --hard && git -C "${rootPath}" pull`
+        const cmd = `git -C "${pluginPath}" reset --hard && git -C "${pluginPath}" pull`
         exec(cmd, (err, stdout, stderr) => {
             if (err) {
                 e.reply('强制更新失败：' + err.message)
