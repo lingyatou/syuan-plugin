924e5dcdb821f607d9a924e6dcf7aaac6efc1042
源Syuan <2331329306@qq.com>
2025-08-23 13:04:09 +0800

    32

--- apps/index.js
+++ apps/index.js
@@ -2,9 +2,11 @@ import { UpdateMemeRepos } from './ssh.js'
 import { WwCheck } from './welcome.js'
 import { OpenCommand } from './gs.js'
 import { Update } from './update.js'
+import { HelpApp } from './help.js'
 
 export const ssh = UpdateMemeRepos
 export const welcome = WwCheck
 export const grasscutter = OpenCommand
 export const updateplugin = Update
-export default [UpdateMemeRepos, WwCheck, grasscutter, updateplugin]
+export const helpApp = HelpApp
+export default [UpdateMemeRepos, WwCheck, grasscutter, updateplugin, HelpApp]
