48f891c085fa1791329135a7f9ccd753dcb89cc4
源Syuan <2331329306@qq.com>
2025-08-31 18:46:37 +0800

    cs

--- apps/index.js
+++ apps/index.js
@@ -3,11 +3,13 @@ import { WwCheck } from './other.js'
 import { OpenCommand } from './gs.js'
 import { Update } from './update.js'
 import { TestPlugin } from './致铸币.js'
+import { cs } from './test.js';
 
 export const ssh = UpdateMemeRepos
 export const welcome = WwCheck
 export const grasscutter = OpenCommand
 export const updateplugin = Update
 export const testPlugin = TestPlugin
+export const CS = cs
 
-export default [UpdateMemeRepos, WwCheck, grasscutter, updateplugin, testPlugin]
+export default [UpdateMemeRepos, WwCheck, grasscutter, updateplugin, testPlugin, CS]
