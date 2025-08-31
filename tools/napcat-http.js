292f976ca05516cc75862c9812bd35553645b920
源Syuan <2331329306@qq.com>
2025-09-01 01:17:01 +0800

    修改napcat-http

--- tools/napcat-http.js
+++ tools/napcat-http.js
@@ -6,7 +6,6 @@ const napcatUrl = 'http://127.0.0.1:3000'
  * 调用 Napcat API
  * @param {string} api - API 接口路径 (例如 "group_poke")
  * @param {Object} data - 提交的表单数据
- * @returns {Promise<Object>} - 返回 axios 的响应结果
  */
 export async function callNapcat(api, data) {
     try {
