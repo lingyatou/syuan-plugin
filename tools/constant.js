bdcdac29da0369d0a9535825435f538362999ee0
源Syuan <2331329306@qq.com>
2025-09-02 11:06:57 +0800

    3

--- tools/constant.js
+++ tools/constant.js
@@ -1,3 +1,3 @@
 
-export const NAPCAT_HTTP_223 = "http://127.0.0.1:223";
-export const NAPCAT_HTTP_304 = "http://127.0.0.1:304";
+export const NAPCAT_HTTP_223 = "http://127.0.0.1:3000";
+export const NAPCAT_HTTP_304 = "http://43.143.104.104:3001";
