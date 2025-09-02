7548609ad76861060e2a432bf96b05cbd8d76fe0
源Syuan <2331329306@qq.com>
2025-09-02 10:34:46 +0800

    3

--- tools/index.js
+++ tools/index.js
@@ -4,6 +4,7 @@ import { versionInfo } from './version.js'
 import { cfgdata } from './cfg.js'
 import { isMaster, sleep } from './admin.js'
 import { NAPCAT_HTTP_223, NAPCAT_HTTP_304 } from './constant.js'
-import { NapCatAPI } from './napcat-http.js'
+import NapCatAPI from './napcat-http.js';
+
 
 export { sshData, pluginPath, rootPath, dataPath, versionInfo, cfgdata, isMaster, sleep, NAPCAT_HTTP_223, NAPCAT_HTTP_304, NapCatAPI }
