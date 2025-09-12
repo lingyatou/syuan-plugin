2a5f737ddc9a7bad5fe600013ee5ce33ab5a41f8
源Syuan <2331329306@qq.com>
2025-09-12 19:10:32 +0800

    2

--- apps/poke.js
+++ apps/poke.js
@@ -40,10 +40,9 @@ export class poke_to_2YM extends plugin {
 
 
     async accept(e) {
-        logger.debug(`[Syuan-Plugin] 收到poke事件: target=${e.target_id}, self=${e.self_id}, group=${e.group_id}`)
         // 仅处理戳账号3999084287的情况
         if (e.target_id != 3999084287) return false
-        if (e.operator_id == e.user_id) return false
+        if (e.operator_id == e.self_id) return false
 
         // 表情包目录：data/Syuan-plugin/Yunzai_image/2YM
         const emojiDir = path.join(rootPath, 'data/Syuan-plugin/Yunzai_image/2YM')
