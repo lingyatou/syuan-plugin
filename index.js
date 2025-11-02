6cf42c19f4a76d887d2c316c55272a8e61b8e034
源Syuan <2331329306@qq.com>
2025-11-02 20:16:56 +0800

    8

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-import { versionInfo } from './tools/index.js'
-export * from './apps/index.js'
+import { versionInfo, pluginPath } from './tools/index.js'
 
-if (!segment?.button)
-    segment.button = () => ""
+import path from 'path'
+export * from './apps/index.js'
+import fs from 'node:fs'
+
+const appsPath = path.join(pluginPath, 'apps')
+
+if (!global.segment) {
+    global.segment = (await import("oicq")).segment
+}
+
+if (!global.core) {
+    try {
+        global.core = (await import("oicq")).core
+    } catch (err) { }
+}
+
+const files = fs.readdirSync(appsPath).filter(file => file.endsWith('.js'))
+let ret = []
+files.forEach((file) => {
+    ret.push(import(`./apps/${file}`))
+})
+ret = await Promise.allSettled(ret)
 
 logger.info('---------```````^_^```````---------')
 logger.info(versionInfo.getProjectName() + versionInfo.getVersion())
 logger.info(versionInfo.getAuthor())
 logger.info(versionInfo.getDescription())
+
+
+logger.info('✨🌸━━━━━━━━ SYUAN-PLUGIN ━━━━━━━━🌸✨')
+logger.info(`💖 插件名称：${logger.cyan(versionInfo.getProjectName())}`)
+logger.info(`📦 插件版本：v${logger.green(versionInfo.getVersion())}`)
+logger.info(`👩‍💻 作者：${logger.magenta(versionInfo.getAuthor())}`)
+logger.info(`📜 描述：${versionInfo.getDescription()}`)
+logger.info('✨🌸━━━━━━━━━━━━━━━━━━━━━━━━━━━━━🌸✨')
+
+let apps = {}
+
+for (let i in files) {
+    let name = files[i].replace('.js', '')
+    if (ret[i].status != 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        logger.error(ret[i].reason)
+        continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+
+export { apps }
